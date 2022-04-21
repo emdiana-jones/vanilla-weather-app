528fe68fd0705654765f0f7391500f316c1c226d
emdiana-jones <103916217+emdiana-jones@users.noreply.github.com>
2022-04-20 23:24:40 -0600

    updated current temperature, wind, and conditions in JS

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let conditionsElement = document.querySelector("#conditions");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  conditionsElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "862505b1b6aac4d71786143d27378ed5";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=boulder&appid=${apiKey}&units=imperial`;
+
+axios.get(apiUrl).then(displayTemperature);
