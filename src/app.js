4e86c860a26e88e631617b942528b03d8409a7ba
emdiana-jones <103916217+emdiana-jones@users.noreply.github.com>
2022-05-01 21:12:24 -0600

    updated forecasting JS with api call

--- src/app.js
+++ src/app.js
@@ -49,6 +49,12 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  let apiKey = "862505b1b6aac4d71786143d27378ed5";
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
